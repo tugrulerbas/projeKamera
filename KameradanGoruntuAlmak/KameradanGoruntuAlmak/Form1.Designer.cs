cf9422bdc6897fbe744a5ad697256468d2eae480
tugrulerbas <tugrulerbas.123@gmail.com>
2017-10-24 02:50:26 +0300

    kamera
    s

---
+++ KameradanGoruntuAlmak/KameradanGoruntuAlmak/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace KameradanGoruntuAlmak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webCamCapture1 = new WebCam_Capture.WebCamCapture();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(623, 462);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Devam Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webCamCapture1
+            // 
+            this.webCamCapture1.CaptureHeight = 240;
+            this.webCamCapture1.CaptureWidth = 320;
+            this.webCamCapture1.FrameNumber = ((ulong)(0ul));
+            this.webCamCapture1.Location = new System.Drawing.Point(0, 0);
+            this.webCamCapture1.Name = "WebCamCapture";
+            this.webCamCapture1.Size = new System.Drawing.Size(342, 252);
+            this.webCamCapture1.TabIndex = 0;
+            this.webCamCapture1.TimeToCapture_milliseconds = 100;
+            this.webCamCapture1.ImageCaptured += new WebCam_Capture.WebCamCapture.WebCamEventHandler(this.webCamCapture1_ImageCaptured);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Çıkış";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(315, 427);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 462);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private WebCam_Capture.WebCamCapture webCamCapture1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ KameradanGoruntuAlmak/KameradanGoruntuAlmak/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KameradanGoruntuAlmak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webCamCapture1.Start(0); //webkamerasından görüntü alınmaya başlanıyor
+        }
+
+        private void webCamCapture1_ImageCaptured(object source, WebCam_Capture.WebcamEventArgs e)
+        {
+            pictureBox1.Image = e.WebCamImage;  //picturebox'un image özelliğine bu olayın
+                                                //WebCamImage özelliği atanarak görüntü akışı sağlanıyor
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webCamCapture1.CaptureWidth = 640; //form yüklenmesi esnasında alınan görüntü genişliği 640px
+            webCamCapture1.CaptureHeight = 480;//form yüklenmesi esnasında alınan görüntü genişliği 640px
+            webCamCapture1.Start(0); //Başlat düğmesine gerek kalmadan form
+                                       //yüklenirken görüntü akışı alınmaya başlanıyor
+            timer1.Enabled = true;  //timer varsayılan olarak devre dışı halde olur, program açılışında
+            //etkindeştiriyoruz
+            timer1.Interval = 20000;//timer1'in tick olayının çalışması için gereken süre (ms)
+            timer2.Enabled = true;//timer varsayılan olarak devre dışı halde olur, program açılışında
+            //etkindeştiriyoruz
+            timer2.Interval = 1000;//timer2'in tick olayının çalışması için gereken süre (ms)
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webCamCapture1.Stop(); //Görüntü akışını durduruyoruz
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close(); //çıkış tuşuna çıkış işlevini ekliyoruz
+        }
+
+        int a=0;
+        int b = 0; //timer2 bloğunda işe yarayacak
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(string.Format("hırsız{0}.png",a)); //picturebox'un image
+                        //özelliğinin save alt özelliğini kullanarak resmi kaydedebiliyoruz.
+            a=a++;  //isimler aynı isimle kaydedilmesin diye tuşa her tıklandığında 1 arttırıyoruz.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(string.Format("hırsız{0}.png", a)); //timer1'in tick olayı
+            a = a++;        //her 20 saniyede bir görüntü kaydetmesi kaydetmesi için formun
+                            //başlangıç olayındaki invertal'e 20000 atadık
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            b = b + 1; //her saniye bir arttırması için
+            if (b < 60) //eğer b, 60'tan küçükse
+            {
+                this.Text = b.ToString() + " saniyedir kayıtta"; //diye yazdırdık
+            }
+            else //değilse yani b 60'tan büyükse dakikayı yazmak için
+                this.Text = (b / 60).ToString() + " dakikadır kayıtta"; //b'yi 60'a bölüp
+            //this.text'e yani Form1.ActiveForm.Text'e, yani form başlığına yazdırdık
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = trackBar1.Value / 100.0; //pencere saydamlığını ayarlamak için
+            //bir trackbar ekledik ve trackbar maximumu 100, minumum'y 10, value'yi 100
+            //yaptık ve 100.0'a böldük
+        }
+    }
+}
